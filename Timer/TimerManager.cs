0c6d774ece2377a5545e60b49d2ca59ccca29dae
Uladzislau_Sakalou <needyoursoul2die@gmail.com>
2015-11-06 01:53:55 +0300

    added TimerTask

---
+++ Timer/TimerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Timer
+{
+    public class TimerManager
+    {
+        public event EventHandler timeOff;
+
+        protected virtual void OnTimeOff()
+        {
+            EventHandler temp = Interlocked.CompareExchange(ref timeOff, null, null);
+            if (temp != null)
+                temp(this, EventArgs.Empty);
+        }
+
+        public void Start(int milliseconds)
+        {
+            Thread.Sleep(milliseconds);
+            OnTimeOff();
+        }
+    }
+}
---
+++ TimerConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timer;
+
+namespace TimerConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimerManager timer = new TimerManager();
+            AlarmClock alarmClock = new AlarmClock(timer);
+            Finisher finisher = new Finisher(timer);
+
+            timer.Start(5000);
+
+            alarmClock.Unregister(timer);
+
+            timer.Start(2000);
+            Console.ReadKey();
+        }
+    }
+}
